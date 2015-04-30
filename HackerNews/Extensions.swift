8c442bba362baa3b77c73896773520880fbc3f09
heyalexchoi <heyalexchoi@gmail.com>
2015-04-30 18:25:38 -0400

    add pmap

--- HackerNews/Extensions.swift
+++ HackerNews/Extensions.swift
@@ -58,3 +58,11 @@ func merge<K,V>(dicts: [K: V]...) -> [K: V] {
     }
     return new
 }
+
+func pmap<T,U>(array: Array<T>, closure: (T) -> U) -> Array<U> {
+    var pmapped = Array<U>()
+    dispatch_apply(array.count, dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0)) { (i) -> Void in
+        pmapped.append(closure(array[i]))
+    }
+    return pmapped
+}
