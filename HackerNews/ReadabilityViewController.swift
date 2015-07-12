db5c90fa30c37ddf95bc06247c5f4c36184d2cd9
heyalexchoi <heyalexchoi@gmail.com>
2015-07-12 04:15:45 -0400

    readability article view controller now uses webview with injected css

--- HackerNews/ReadabilityViewController.swift
+++ HackerNews/ReadabilityViewController.swift
@@ -6,8 +6,6 @@
 //  Copyright (c) 2015 Alex Choi. All rights reserved.
 //
 
-import DTCoreText
-
 class ReadabilityViewContoller: UIViewController {
     
     var article: ReadabilityArticle?
@@ -17,7 +15,7 @@ class ReadabilityViewContoller: UIViewController {
     let story: Story
     let articleURL: NSURL
     
-    let textView = DTAttributedTextView()
+    let webView = UIWebView()
     
     init(story: Story) {
         self.story = story
@@ -26,19 +24,20 @@ class ReadabilityViewContoller: UIViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: "actionButtonDidPress")
         
-        textView.textDelegate = self
-        textView.backgroundColor = UIColor.backgroundColor()
-        for view in [textView] {
+        // hides white flash. for whatever reason setting webview's background color doesnt prevent white flash.
+        view.backgroundColor = UIColor.backgroundColor()
+        webView.opaque = false
+        webView.backgroundColor = UIColor.clearColor()
+        
+        for view in [webView] {
             view.setTranslatesAutoresizingMaskIntoConstraints(false)
             self.view.addSubview(view)
         }
         
         view.twt_addConstraintsWithVisualFormatStrings([
-            "H:|[textView]|",
-            "V:|[textView]|"], views: [
-                "textView": textView])
-        
-        populateArticleInfo()
+            "H:|[webView]|",
+            "V:|[webView]|"], views: [
+                "webView": webView])
         
         getReadabilityArticle()
     }
@@ -47,33 +46,8 @@ class ReadabilityViewContoller: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        let inset: CGFloat = 20
-        textView.contentInset = UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
-    }
-    
     deinit {
         task?.cancel()
-    }
-    
-    func populateArticleInfo() {
-        let articleInfo = NSMutableAttributedString()
-        
-        let title = NSMutableAttributedString(string: story.title, attributes: merge(TextAttributes.titleAttributes, TextAttributes.centerAlignment))
-        articleInfo.appendAttributedString(title)
-        
-        let submitter = NSAttributedString(string: "\n\nsubmitted \(story.date.timeAgoSinceNow()) by \(story.by)", attributes: merge(TextAttributes.textReaderAttributes, TextAttributes.centerAlignment))
-        articleInfo.appendAttributedString(submitter)
-        
-        let hyperlink = articleURL.hyperlink()
-        let linkData = hyperlink.dataUsingEncoding(NSUTF8StringEncoding)
-        let link = NSAttributedString(HTMLData: linkData, options: [DTDefaultFontName: UIFont.textReaderFont().fontName, DTDefaultFontSize: UIFont.textReaderFont().pointSize, DTDefaultLinkColor: UIColor.tintColor()], documentAttributes: nil)
-        articleInfo.appendAttributedString(NSAttributedString(string: "\n\n"))
-        articleInfo.appendAttributedString(link)
-        
-        textView.attributedString = articleInfo
     }
     
     func getReadabilityArticle() {
@@ -95,81 +69,61 @@ class ReadabilityViewContoller: UIViewController {
     }
     
     func finishLoadingArticle(article: ReadabilityArticle) {
-        let attributedContent = NSMutableAttributedString(attributedString: article.attributedContent)
-        let attributedText = NSMutableAttributedString()
-        attributedText.appendAttributedString(textView.attributedString)
-        attributedText.appendAttributedString(NSAttributedString(string: "\n\n", attributes: TextAttributes.textReaderAttributes))
         
-        let paragraphStyle = NSMutableParagraphStyle() // work around. DTAttributedTextView doesn't seem to display line spacing correctly
-        paragraphStyle.minimumLineHeight = UIFont.textReaderFont().pointSize * 1.3
-        attributedContent.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSRange(location: 0, length: attributedContent.length))
-        attributedText.appendAttributedString(attributedContent)
-
-        textView.attributedString = attributedText
+        let cssPath = NSBundle.mainBundle().pathForResource("readability_article", ofType: "css")!
+        let css = String(contentsOfFile:cssPath, encoding: NSUTF8StringEncoding, error: nil)!
+        
+        // swift compiler choking on string concatenations. had to break them into more statements
+        var customCSS =
+        
+        "body {"
+        customCSS +=
+            "font-size: \(UIFont.textReaderFont().pointSize);" +
+            "font-family: \(UIFont.textReaderFont().fontName);" +
+            "background-color: \(UIColor.backgroundColor().hexString());" +
+            "color: \(UIColor.textColor().hexString());"
+        customCSS +=
+        " }"
+        
+        customCSS +=
+        "a {" +
+            "color: \(UIColor.textColor().hexString());" +
+        "}"
+        
+        let head = "<head><style type='text/css'>" + css + customCSS + "</style></head>"
+        
+        var articleInfo =
+        "<div class='article_info'>" +
+        "<h1>\(article.title)</h1>"
+        
+        if !article.author.isEmpty {
+            articleInfo +=
+                "<div>" +
+                "<span class='by'>By </span>" +
+                "<span class='author'>\(article.author), </span>" +
+                "<span class='domain'>\(article.domain)</span>" +
+            "</div>"
+        }
+        
+        if let date = article.datePublished {
+            let string = DateFormatter.stringFromDate(date, format: "MMM d, yyyy")
+            articleInfo += "<div>\(string)</div>"
+        }
+        
+        articleInfo +=
+            "<div><a href='url'> \(article.URL) </a></div>" +
+        "</div>"
+        
+        let body = "<body>" + articleInfo + article.content + "</body>"
+        
+        let html = head + body
+                
+        webView.loadHTMLString(html, baseURL: nil)
     }
     
     func actionButtonDidPress() {
-        let storyActivity = StoryActivity()        
+        let storyActivity = StoryActivity()
         presentViewController(UIActivityViewController(activityItems: [articleURL, story], applicationActivities: [storyActivity]), animated: true, completion: nil)
     }
     
-}
-
-
-extension ReadabilityViewContoller: UITextViewDelegate {
-    
-    func textView(textView: UITextView, shouldInteractWithURL URL: NSURL, inRange characterRange: NSRange) -> Bool {
-        presentViewController(UINavigationController(rootViewController: WebViewController(url: URL)), animated: true, completion: nil)
-        return false
-    }
-    
-}
-
-extension ReadabilityViewContoller: DTAttributedTextContentViewDelegate, DTLazyImageViewDelegate, DTWebVideoViewDelegate {
-    
-    func attributedTextContentView(attributedTextContentView: DTAttributedTextContentView!, viewForAttachment attachment: DTTextAttachment!, frame: CGRect) -> UIView! {
-        if attachment.isKindOfClass(DTImageTextAttachment.self) {
-            let imageView = DTLazyImageView(frame: frame)
-            imageView.delegate = self
-            imageView.url = attachment.contentURL
-            return imageView
-        } else if attachment.isKindOfClass(DTIframeTextAttachment.self) {
-            if attachment.displaySize.width > attributedTextContentView.bounds.size.width {
-                let scale = frame.size.width / attributedTextContentView.bounds.size.width
-                attachment.displaySize = CGSize(width: attachment.displaySize.width / scale, height: attachment.displaySize.height / scale)
-            }
-            let videoView = DTWebVideoView(frame: frame)
-            videoView.attachment = attachment
-            return videoView
-        }
-        return nil
-    }
-    
-    func lazyImageView(lazyImageView: DTLazyImageView!, didChangeImageSize size: CGSize) {
-        let url = lazyImageView.url
-        let predicate = NSPredicate(format: "contentURL == %@", url)
-        let textContentView = textView.attributedTextContentView
-        for attachment in textContentView.layoutFrame.textAttachmentsWithPredicate(predicate) as! [DTTextAttachment] {
-            attachment.originalSize = size
-            if attachment.displaySize.width > textContentView.layoutFrame.frame.size.width {
-                let resizeRatio = textContentView.layoutFrame.frame.size.width / attachment.displaySize.width
-                attachment.displaySize = CGSize(width: attachment.displaySize.width * resizeRatio, height: attachment.displaySize.height * resizeRatio)
-            }
-            textContentView.layouter = nil
-            textContentView.relayoutText()
-        }
-    }
-    
-    func attributedTextContentView(attributedTextContentView: DTAttributedTextContentView!, viewForLink url: NSURL!, identifier: String!, frame: CGRect) -> UIView! {
-        let linkButton = DTLinkButton(frame: frame)
-        linkButton.URL = url
-        linkButton.minimumHitSize = CGSize(width: 25, height: 25)
-        linkButton.addTarget(self, action: "linkButtonDidPress:", forControlEvents: .TouchUpInside)
-        linkButton.GUID = identifier
-        return linkButton
-    }
-    
-    func linkButtonDidPress(button: DTLinkButton) {
-        presentViewController(UINavigationController(rootViewController: WebViewController(url: button.URL)), animated: true, completion: nil)
-    }
 }
