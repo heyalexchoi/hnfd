8b7e0bc2c563c9de466913f372634d4f0ef287b8
heyalexchoi <heyalexchoi@gmail.com>
2015-09-27 15:34:39 -0400

    don't display error message on cancel

--- HackerNews/ErrorController.swift
+++ HackerNews/ErrorController.swift
@@ -11,12 +11,18 @@ import TSMessages
 class ErrorController {
     
     class func showErrorNotification(error: NSError) {
+        if error.code == NSURLErrorCancelled {
+            return
+        }
         if let rootNavigationController = UIApplication.sharedApplication().delegate?.window??.rootViewController {
+            
             var title = error.localizedDescription
+            
             if let userInfo = error.userInfo as? [String: AnyObject],
                 let messages = userInfo[Public.Constants.errorMessagesKey] as? String {
                 title = messages
             }
+            
             TSMessage.showNotificationInViewController(rootNavigationController, title: title, subtitle:nil , image: nil, type: .Error, duration: 2, callback: nil, buttonTitle: nil, buttonCallback: nil, atPosition: .NavBarOverlay, canBeDismissedByUser: true)
         }
     }
