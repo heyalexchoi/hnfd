0ba38b20b8ac30fd1f827a5d87be34c3da218a53
heyalexchoi <heyalexchoi@gmail.com>
2016-05-05 19:15:57 -0400

    added router and api client

--- HackerNews/APIClient.swift
+++ HackerNews/APIClient.swift
@@ -16,16 +16,88 @@ struct APIClient {
         return Alamofire.Manager(configuration: configuration)
     }()
     
-    // need router for building requests
-    // need way to get destination url that will pop the file into where pincache is reading
-    
     func download(request: URLRequestConvertible, destinationURL: NSURL) {
         let downloadDestination: Request.DownloadFileDestination = { (_,_) -> NSURL in
             return destinationURL
         }
         backgroundManager.download(request, destination: downloadDestination)
+    }    
+}
+
+enum HNFDRouter: URLRequestConvertible {
+    
+    case Stories(type: StoriesType)
+    case Story(id: Int)
+    
+    var method: Alamofire.Method {
+        switch self {
+        default:
+            return .GET
+        }
     }
     
+    var path: String {
+        switch self {
+        case .Stories(let type):
+            return "\(type.rawValue)"
+        case .Story(let id):
+            return "/items/\(id)"
+        }
+    }
+        
+    var parameters: [String: AnyObject] {
+        switch self {
+        default:
+            return [:]
+        }
+    }
     
+    // MARK: URLRequestConvertible
     
+    var URLRequest: NSMutableURLRequest {
+        let URL = NSURL(string: Private.Constants.HNAPIBaseURLString)!
+        let mutableURLRequest = NSMutableURLRequest(URL: URL.URLByAppendingPathComponent(path))
+        mutableURLRequest.HTTPMethod = method.rawValue
+        //            mutableURLRequest.setValue("", forHTTPHeaderField: "field") // clever header shit?
+        return Alamofire.ParameterEncoding.URL.encode(mutableURLRequest, parameters: parameters).0
+    }
+}
+
+enum ReadabilityRouter: URLRequestConvertible {
+    
+    case Article(URL: NSURL)
+    
+    var method: Alamofire.Method {
+        switch self {
+        default:
+            return .GET
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .Article:
+            return "/content/v1/parser"
+        }
+    }
+    
+    var parameters: [String: AnyObject] {
+        switch self {
+        case .Article(let URL):
+            return [
+                "url": URL,
+                "token": Private.Keys.readabilityParserAPIToken
+            ]
+        }
+    }
+    
+    // MARK: URLRequestConvertible
+    
+    var URLRequest: NSMutableURLRequest {
+        let URL = NSURL(string: "https://readability.com/api")!
+        let mutableURLRequest = NSMutableURLRequest(URL: URL.URLByAppendingPathComponent(path))
+        mutableURLRequest.HTTPMethod = method.rawValue
+        //            mutableURLRequest.setValue("", forHTTPHeaderField: "field") // clever header shit?
+        return Alamofire.ParameterEncoding.URL.encode(mutableURLRequest, parameters: parameters).0
+    }
 }
