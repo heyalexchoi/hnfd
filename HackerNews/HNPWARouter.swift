4c62dd82c175e5baee8b6b620aafa83ae418224d
alex choi <heyalexchoi@gmail.com>
2019-02-23 16:04:44 -0500

    hnpwa router

--- HackerNews/HNPWARouter.swift
+++ HackerNews/HNPWARouter.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import Alamofire
+
+enum HNPWARouter: URLRequestConvertible {
+    
+    case item(id: Int)
+    
+    var method: Alamofire.HTTPMethod {
+        switch self {
+        default:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .item(let id):
+            return "/v0/item/\(id).json"
+        }
+    }
+    
+    var parameters: [String: Any] {
+        switch self {
+        default:
+            return [:]
+        }
+    }
+    
+    // MARK: URLRequestConvertible
+    
+    func asURLRequest() throws -> URLRequest {
+        let url = URL(string: "https://api.hnpwa.com/")!.appendingPathComponent(path)
+        var request = try URLEncoding.default.encode(URLRequest(url: url), with: parameters)
+        request.httpMethod = method.rawValue
+        return request
+    }
+}
+
