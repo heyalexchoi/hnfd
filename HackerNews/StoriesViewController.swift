51b288ca15de1ec12e433ec63c9341709e534ae1
alex choi <heyalexchoi@gmail.com>
2019-02-22 21:23:28 -0500

    added infinite scrolling

--- HackerNews/StoriesViewController.swift
+++ HackerNews/StoriesViewController.swift
@@ -23,6 +23,8 @@ class StoriesViewController: UIViewController {
     fileprivate let titleView = StoriesTitleView()
     fileprivate let menu = REMenu()
     
+    fileprivate let defaultStoryRequestLimit = 25
+    
     override var title: String? {
         didSet {
             titleView.title = title
@@ -61,11 +63,12 @@ class StoriesViewController: UIViewController {
         
         getStories(showHUD: true)
         loadPinnedStories()
-        tableView.addInfiniteScroll { (tableView) -> Void in
-            // update table view
-            
-            // finish infinite scroll animation
-            tableView.finishInfiniteScroll()
+        
+        tableView.addInfiniteScrolling { [weak self] in
+            if let self = self {
+                let offset = self.stories.count
+                self.getStories(offset: offset)
+            }
         }
     }
 }
@@ -74,15 +77,17 @@ class StoriesViewController: UIViewController {
 
 extension StoriesViewController {
     
-    fileprivate func loadStories(_ stories: [Story], scrollToTop: Bool, showHUD: Bool) {
+    fileprivate func loadStories(_ stories: [Story], appendStories: Bool, scrollToTop: Bool, showHUD: Bool) {
         
         ProgressHUD.hideAllHUDs(for: tableView, animated: true)
         tableView.pullToRefreshView.stopAnimating()
 
         title = storiesType.title
-        self.stories = stories
+        
+        self.stories = appendStories ? self.stories + stories : stories
         
         tableView.reloadData()
+        tableView.infiniteScrollingView?.stopAnimating()
         
         if scrollToTop {
             tableView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
@@ -91,16 +96,14 @@ extension StoriesViewController {
         _ = DataSource.fullySync(stories: stories, timeout: 2)
     }
     
-    fileprivate func getStories(scrollToTop: Bool = false, showHUD: Bool = false) {
+    fileprivate func getStories(offset: Int = 0, scrollToTop: Bool = false, showHUD: Bool = false) {
+        let appendStories = offset == 0 ? false : true
         if showHUD {
             ProgressHUD.showAdded(to: tableView, animated: true)
         }
-        // // wa wa wee waa
-        let limit = 25
-        let offset = 0
-        DataSource.getStories(withType: storiesType, limit: limit, offset: offset)
+        DataSource.getStories(withType: storiesType, limit: defaultStoryRequestLimit, offset: offset)
         .then { (stories) -> Void in
-            self.loadStories(stories, scrollToTop: scrollToTop, showHUD: showHUD)
+            self.loadStories(stories, appendStories: appendStories, scrollToTop: scrollToTop, showHUD: showHUD)
         }
         .catch { (error) in
             ErrorController.showErrorNotification(error)
